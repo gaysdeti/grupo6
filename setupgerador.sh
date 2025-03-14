2e471ca41452e5f6efb27698448357143cbcf5c9
nnfabi-m <166771398+nnfabi-m@users.noreply.github.com>
2025-03-13 21:43:13 -0300

    Update setupgerador.sh

--- setupgerador.sh
+++ setupgerador.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
+
+# Atualizar os pacotes
+apt-get update
+
+# Instalar dependências do Python
+apt-get install -y python3 python3-pip
+
+# Instalar pacotes Python necessários
+pip3 install pandas numpy
+
+# Executar o gerador de dados
 python3 /vagrant/gerador_dados.py
+
+echo "Instalação e execução do gerador de dados concluídas!"
